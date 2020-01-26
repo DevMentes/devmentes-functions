d65ebb523a23d1d25ff355519b929552955275f1
camilo@dill.cl-at-143347831018 <kmilo93sd@gmail.com>
2020-01-26 13:37:25 -0300

    working

---
+++ posts-api/mocks/writers.py
@@ -0,0 +1,8 @@
+writers = [
+    {
+        "id": "asdadasdas",
+        "name": "McBolas",
+        "avatar": "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png",
+        "followers": 235,
+        "posts": 6
+    }]
